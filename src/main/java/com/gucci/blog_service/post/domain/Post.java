f696930180bfdb64a93283c78eaa44a6df4ec0ad
hyeonji91 <hjkim1770@gmail.com>
2025-04-07 20:54:54 +0900

    Feat : BaseEntity작성

--- src/main/java/com/gucci/blog_service/post/domain/Post.java
+++ src/main/java/com/gucci/blog_service/post/domain/Post.java
@@ -1,5 +1,6 @@
 package com.gucci.blog_service.post.domain;
 
+import com.gucci.blog_service.config.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import org.hibernate.annotations.ColumnDefault;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Post { //todo: user연결하기
+public class Post extends BaseEntity { //todo: user연결하기
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long postId;
