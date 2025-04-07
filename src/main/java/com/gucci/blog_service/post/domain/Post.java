645db6c9c2869b57c936116ca4fdd0fe2f91a623
hyeonji91 <hjkim1770@gmail.com>
2025-04-07 19:41:48 +0900

    Feat : comment, post 기본 entity 세팅

---
+++ src/main/java/com/gucci/blog_service/post/domain/Post.java
@@ -0,0 +1,26 @@
+package com.gucci.blog_service.post.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long postId;
+
+    @Column(nullable = false)
+    private String title;
+
+    private Long documentId;
+
+    @ColumnDefault("0")
+    @Column(nullable = false)
+    private Long view;
+}
