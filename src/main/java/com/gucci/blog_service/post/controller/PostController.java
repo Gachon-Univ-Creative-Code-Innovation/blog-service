b20e768acb799daf95c23a6a2be6fae645498faa
hyeonji91 <hjkim1770@gmail.com>
2025-04-14 21:15:46 +0900

    Feat : one post read  API

--- src/main/java/com/gucci/blog_service/post/controller/PostController.java
+++ src/main/java/com/gucci/blog_service/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.gucci.blog_service.post.controller;
 
 import com.gucci.blog_service.post.domain.Post;
 import com.gucci.blog_service.post.domain.dto.PostRequestDTO;
+import com.gucci.blog_service.post.domain.dto.PostResponseDTO;
 import com.gucci.blog_service.post.service.PostService;
 import com.gucci.common.response.ApiResponse;
 import jakarta.validation.Valid;
@@ -22,5 +23,13 @@ public class PostController {
     ){
         Post newPost = postService.createPost(token, post);
         return ApiResponse.success(newPost.getPostId() + " 글이 정상적으로 생성되었습니다.");
+    }
+
+    @GetMapping("/{postId}")
+    public ApiResponse<PostResponseDTO.GetPostDetail> getPostDetail(
+            @PathVariable Long postId
+    ){
+        PostResponseDTO.GetPostDetail getPostDetail = postService.getPostDetail(postId);
+        return ApiResponse.success(getPostDetail);
     }
 }
