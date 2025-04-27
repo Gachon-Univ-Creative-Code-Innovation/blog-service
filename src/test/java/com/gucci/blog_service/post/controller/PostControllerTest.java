63351a15a956e043760ecb8c1b21c18b9a3de241
hyeonji91 <hjkim1770@gmail.com>
2025-04-28 01:22:56 +0900

    Feat : 임시저장 controller test 작성

--- src/test/java/com/gucci/blog_service/post/controller/PostControllerTest.java
+++ src/test/java/com/gucci/blog_service/post/controller/PostControllerTest.java
@@ -7,6 +7,7 @@ import com.gucci.blog_service.post.domain.dto.PostResponseDTO;
 import com.gucci.blog_service.post.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,6 +16,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -38,6 +41,9 @@ public class PostControllerTest {
 
     private final String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikh5ZW9uamkiLCJpYXQiOjE1MTYyMzkwMjIsInVzZXJfaWQiOjF9.UnBPcqiYQW7jQsP23-w5FwOfi1yj2VImffJl6nq_nZY";
 
+    /**
+     * 게시글
+     */
     @Test
     @DisplayName("게시글 생성 테스트")
     void createPostTest() throws Exception {
@@ -76,6 +82,7 @@ public class PostControllerTest {
         mockMvc.perform(get("/api/blog-service/posts/{postId}", postId))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("요청이 성공적으로 처리되었습니다."))
                 .andExpect(jsonPath("$.data.postId").value(postId));
     }
 
@@ -100,6 +107,7 @@ public class PostControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("요청이 성공적으로 처리되었습니다."))
                 .andExpect(jsonPath("$.data").value(post.getPostId()+" 글이 정상적으로 수정되었습니다."));
     }
 
@@ -114,8 +122,98 @@ public class PostControllerTest {
                 .header("Authorization", token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("요청이 성공적으로 처리되었습니다."))
                 .andExpect(jsonPath("$.data").value("글이 정상적으로 삭제되었습니다."));
     }
+
+
+    /**
+     * 임시저장
+     */
+    @Test
+    @DisplayName("임시저장 생성 테스트")
+    void createDraftTest() throws Exception {
+        PostRequestDTO.createDraft request = PostRequestDTO.createDraft.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        Post post = Post.builder()
+                .postId(1L)
+                .build();
+
+        Mockito.when(postService.createDraft(anyString(), any())).thenReturn(post);
+
+        mockMvc.perform(post("/api/blog-service/posts/drafts")
+                .header("Authorization", token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("요청이 성공적으로 처리되었습니다."))
+                .andExpect(jsonPath("$.data").value(post.getPostId() + " 글이 임시저장되었습니다"));
+    }
+
+    @Test
+    @DisplayName("임시저장 글 상세조회")
+    void getDraftDetailTest() throws Exception {
+        Long draftPostId = 1L;
+        PostResponseDTO.GetDraftDetail response = PostResponseDTO.GetDraftDetail.builder()
+                .draftPostId(draftPostId)
+                .build();
+
+        Mockito.when(postService.getDraftDetail(anyString(), anyLong())).thenReturn(response);
+
+        mockMvc.perform(get("/api/blog-service/posts/drafts/{draftPostId}", draftPostId)
+                .header("Authorization", token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("요청이 성공적으로 처리되었습니다."))
+                .andExpect(jsonPath("$.data.draftPostId").value(response.getDraftPostId()));
+    }
+
+    @Test
+    @DisplayName("임시저장 리스트 조회")
+    void getDraftListTest() throws Exception {
+        PostResponseDTO.GetDraft draft1 = PostResponseDTO.GetDraft.builder()
+                .draftPostId(1L)
+                .build();
+        PostResponseDTO.GetDraft draft2 = PostResponseDTO.GetDraft.builder()
+                .draftPostId(2L)
+                .build();
+        PostResponseDTO.GetDraftList draftList = PostResponseDTO.GetDraftList.builder()
+                .draftList(List.of(draft1, draft2))
+                .build();
+
+        Mockito.when(postService.getDraftList(anyString())).thenReturn(draftList);
+
+        mockMvc.perform(get("/api/blog-service/posts/drafts")
+                .header("Authorization", token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("요청이 성공적으로 처리되었습니다."))
+                .andExpect(jsonPath("$.data.draftList").isArray())
+                .andExpect(jsonPath("$.data.draftList.length()").value(2))
+                .andExpect(jsonPath("$.data.draftList[0].draftPostId").value(1L))
+                .andExpect(jsonPath("$.data.draftList[1].draftPostId").value(2L));
+    }
+
+    @Test
+    @DisplayName("임시저장 삭제 테스트")
+    void deleteDraftTest() throws Exception {
+        Long draftPostId = 1L;
+
+        Mockito.doNothing().when(postService).deleteDraft(anyString(), anyLong());
+
+        mockMvc.perform(delete("/api/blog-service/posts/drafts/{draftPostId}", draftPostId)
+        .header("Authorization", token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("요청이 성공적으로 처리되었습니다."))
+                .andExpect(jsonPath("$.data").value("임시저장 글 삭제 완료"));
+
+    }
+
+
 
 }
 
