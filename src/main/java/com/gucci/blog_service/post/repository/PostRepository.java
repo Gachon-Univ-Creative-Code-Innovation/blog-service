a1bb8a69198aab3bbc2986d0ce8e33b391f39df4
hyeonji91 <hjkim1770@gmail.com>
2025-04-27 21:29:58 +0900

    Refactor : 글 삭제  API 임시저장 고려

--- src/main/java/com/gucci/blog_service/post/repository/PostRepository.java
+++ src/main/java/com/gucci/blog_service/post/repository/PostRepository.java
@@ -4,8 +4,10 @@ import com.gucci.blog_service.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByUserId(Long userId);
+    Optional<Post> findByParentPostId(Long parentPostId);
 }
