87805f7af2f18997ec6966595026583ed4db13f3
hyeonji91 <hjkim1770@gmail.com>
2025-05-04 05:43:44 +0900

    Feat : createPost API에 category추가

---
+++ src/main/java/com/gucci/blog_service/category/service/CategoryService.java
@@ -0,0 +1,24 @@
+package com.gucci.blog_service.category.service;
+
+import com.gucci.blog_service.category.domain.Category;
+import com.gucci.blog_service.category.domain.type.CategoryType;
+import com.gucci.blog_service.category.repository.CategoryRepository;
+import com.gucci.common.exception.CustomException;
+import com.gucci.common.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public Category getCategory(Long id) {
+        if (id == null) {
+            return categoryRepository.findById(CategoryType.ETC.getCode())
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+        }
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+    }
+}
