ba7720302e5fb0511e66599e84b4b5a2567d4285
hyeonji91 <hjkim1770@gmail.com>
2025-05-12 18:58:43 +0900

    Feat : 전체글 조회

--- src/main/java/com/gucci/blog_service/comment/controller/CommentController.java
+++ src/main/java/com/gucci/blog_service/comment/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.gucci.blog_service.comment.domain.dto.CommentRequestDTO;
 import com.gucci.blog_service.comment.domain.dto.CommentResponseDTO;
 import com.gucci.blog_service.comment.service.CommentService;
 import com.gucci.common.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +20,10 @@ public class CommentController {
 
     @PostMapping("")
     public ApiResponse<String> createComment(
-            @RequestHeader("Authorization") String token,
+            HttpServletRequest request,
             @RequestBody @Valid CommentRequestDTO.CreateComment createComment
     ) {
+        String token = request.getHeader("Authorization");
         Comment comment = commentService.createComment(createComment, token);
         return ApiResponse.success(comment.getCommentId()+" 댓글이 정상적으로 생성되었습니다");
     }
@@ -35,18 +37,21 @@ public class CommentController {
 
     @PatchMapping("/{commentId}")
     public ApiResponse<String> modifyComments(
-            @RequestHeader("Authorization") String token,
+            HttpServletRequest request,
             @PathVariable Long commentId,
             @RequestBody @Valid CommentRequestDTO.UpdateComment updateComment
     ) {
+        String token = request.getHeader("Authorization");
         Comment comment = commentService.updateComment(commentId, updateComment, token);
         return ApiResponse.success(comment.getCommentId() + " 댓글 정상적으로 업데이트를 완료했습니다");
     }
 
     @DeleteMapping("/{commentId}")
     public ApiResponse<String> deleteComments(
-            @RequestHeader("Authorization") String token,
-            @PathVariable Long commentId) {
+            HttpServletRequest request,
+            @PathVariable Long commentId
+    ) {
+        String token = request.getHeader("Authorization");
         commentService.deleteComment(commentId, token);
         return ApiResponse.success("댓글 삭제 완료");
     }
