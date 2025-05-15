7e472c082e718b4d29345818a97d84716f9705ce
hyeonji91 <hjkim1770@gmail.com>
2025-05-15 16:15:46 +0900

    Feat : elastic serach entity 정의

---
+++ src/main/java/com/gucci/blog_service/post/domain/PostSerach.java
@@ -0,0 +1,28 @@
+package com.gucci.blog_service.post.domain;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "post")
+public class PostSerach {
+    @Id
+    private String id;
+
+
+    private String title;
+    private String author;
+    private List<String> tags;
+    private String content;
+    private LocalDateTime createdAt;
+}
