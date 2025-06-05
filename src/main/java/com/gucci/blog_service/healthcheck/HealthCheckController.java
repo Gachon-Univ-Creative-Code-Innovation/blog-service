29823e75ae4f72daf23b81451b6ad8bb8f6ce36f
msj9965 <msj9965@gmail.com>
2025-06-05 15:18:09 +0900

    feat: ci file 작성

---
+++ src/main/java/com/gucci/blog_service/healthcheck/HealthCheckController.java
@@ -0,0 +1,19 @@
+package com.gucci.blog_service.healthcheck;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/blog-service")
+@RequiredArgsConstructor
+public class HealthCheckController {
+    private final Environment environment;
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "Blog Service is up and running on port: " + environment.getProperty("local.server.port");
+    }
+}
