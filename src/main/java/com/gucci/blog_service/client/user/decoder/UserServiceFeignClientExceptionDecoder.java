8dab48985642ef01264232ce02c36bb6eafbcaca
hyeonji91 <hjkim1770@gmail.com>
2025-06-06 21:10:33 +0900

    Feat : 디버깅 코드 추가

--- src/main/java/com/gucci/blog_service/client/user/decoder/UserServiceFeignClientExceptionDecoder.java
+++ src/main/java/com/gucci/blog_service/client/user/decoder/UserServiceFeignClientExceptionDecoder.java
@@ -4,20 +4,24 @@ import com.gucci.common.exception.CustomException;
 import com.gucci.common.exception.ErrorCode;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserServiceFeignClientExceptionDecoder implements ErrorDecoder {
+	private final Logger logger = LoggerFactory.getLogger(UserServiceFeignClientExceptionDecoder.class);
+
 	@Override
 	public Exception decode(String methodKey, Response response) {
 		if (response.status() >= 400 && response.status() <= 499) {
-			System.out.println("error "+response);
+			logger.debug("error "+response);
 			return switch (response.status()) {
 				case 400 -> new CustomException(ErrorCode.FAIL);
 				default -> new CustomException(ErrorCode.FAIL);
 			};
 		} else {
-			System.out.println("error "+response);
-			System.out.println("error "+response.reason());
-			System.out.println("error "+response.request());
+			logger.debug("error response"+response);
+			logger.debug("errorr esponse.reason() "+response.reason());
+			logger.debug("error response.request()"+response.request());
 
 			return new CustomException(ErrorCode.FAIL);
 		}
